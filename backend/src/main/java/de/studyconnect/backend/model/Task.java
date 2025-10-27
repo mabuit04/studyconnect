73149d2aa0e3932e13339a0b547e78a25d2b6801
Linus <lisait00@hs-esslingen.de>
2025-10-27 17:35:16 +0100

    Create Task.java and Task Status enumeration to the model (entities)

---
+++ backend/src/main/java/de/studyconnect/backend/model/Task.java
@@ -0,0 +1,31 @@
+package de.studyconnect.backend.model;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "tasks")
+@Getter
+@Setter
+public class Task extends BaseEntity {
+
+    private String title;
+
+    private String description;
+
+    @Enumerated(EnumType.STRING)
+    private TaskStatus status;
+
+    private LocalDate dueDate;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "assignee_id")
+    private User assignee;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "group_id")
+    private Group group;
+}
---
+++ backend/src/main/java/de/studyconnect/backend/model/TaskStatus.java
@@ -0,0 +1,7 @@
+package de.studyconnect.backend.model;
+
+public enum TaskStatus {
+    TODO,
+    IN_PROGRESS,
+    DONE
+}
