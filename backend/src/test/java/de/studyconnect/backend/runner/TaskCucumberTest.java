ae0f9d15cb4b23977f291a474e73b9bbd212d1bd
mabuit04 <mabuit04@hs-esslingen.de>
2025-11-10 17:01:04 +0100

    pom.xml edited, TaskCucumberTest umbenannt

--- backend/src/test/java/de/studyconnect/backend/runner/TaskCucumberTest.java
+++ backend/src/test/java/de/studyconnect/backend/runner/TaskCucumberTest.java
@@ -32,5 +32,5 @@ import static io.cucumber.junit.platform.engine.Constants.GLUE_PROPERTY_NAME;
 @SelectClasspathResource("features")
 @ConfigurationParameter(key = GLUE_PROPERTY_NAME, value = "de.studyconnect.backend.steps")
 
-public class TaskCucumberIT {
+public class TaskCucumberTest {
 }
--- backend/src/test/resources/features/Task.feature
+++ backend/src/test/resources/features/Task.feature
@@ -1,6 +1,7 @@
 Feature: Task Operations
 
-Background: User Hugo is logged in as a standard user
+Background: 
+    Given User Hugo is logged in as a standard user
 
     Scenario: Create task
         Given user Hugo has a valid account
